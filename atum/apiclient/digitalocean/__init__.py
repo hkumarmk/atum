e2097bf2ec3a89757f16eecc458d8d377145d074
hkumarmk <hkumar@d4devops.org>
2016-05-08 00:00:48 +0000

    Fixing import errors because of cyclic dep

--- atum/apiclient/digitalocean/__init__.py
+++ atum/apiclient/digitalocean/__init__.py
@@ -1,7 +1,4 @@
-from atum.apiclient.digitalocean.flavor import Flavor
 from atum.apiclient.common import BaseConnection, BaseAPIClient
-
-DIGITALOCEAN_ENDPOINT = "https://api.digitalocean.com/v2/"
 
 
 class Connection(BaseConnection):
@@ -18,17 +15,3 @@ class APIClient(BaseAPIClient):
         super(APIClient, self).__init__(connection)
 
 
-class DigitalOcean(object):
-
-    def __init__(self, connection):
-        self.flavor = Flavor(connection)
-
-
-def get_client(auth, endpoint=DIGITALOCEAN_ENDPOINT):
-    """ get_client is the gateway to the provider.
-    :param auth: A dictionary with appropriate keys.
-    :param endpoint:
-    :return:
-    """
-    connection = Connection(endpoint, auth)
-    return DigitalOcean(connection)
